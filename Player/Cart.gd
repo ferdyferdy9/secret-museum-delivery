2a22bdbaaf4182c7181efa987bbcc875a3aad455
ferdyferdy9 <ferdyferdy9514@gmail.com>
2020-10-30 23:09:29 +0700

    Add Cart

---
+++ Player/Cart.gd
@@ -0,0 +1,29 @@
+extends RigidBody2D
+
+onready var anim_tree = $AnimationTree
+
+var target:RigidBody2D
+
+func _ready():
+	anim_tree.active = true
+
+
+func _process(delta):
+	update_face()
+
+
+func _physics_process(delta):
+	if target == null:
+		return
+	
+	if target.global_position.distance_squared_to(global_position) > 400:
+		var desired_vec:Vector2 = (target.global_position - global_position).normalized() * 20
+		var desired_position:Vector2 = target.global_position - desired_vec
+		
+		var force = lerp(global_position, desired_position, 0.1) - global_position
+		apply_central_impulse(force)
+		target.apply_central_impulse(-force * mass)
+
+
+func update_face(): 
+	anim_tree.set("parameters/facing/blend_position", linear_velocity.normalized())
