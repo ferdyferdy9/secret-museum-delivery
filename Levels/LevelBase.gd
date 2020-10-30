463907be99475515e6ac342b442bd31916a02ff7
ferdyferdy9 <ferdyferdy9514@gmail.com>
2020-10-30 23:10:32 +0700

    Update LevelBase

--- Levels/LevelBase.gd
+++ Levels/LevelBase.gd
@@ -1,5 +1,28 @@
 extends Node
 
+onready var ysort = $YSort
+
+const cart_scene = preload("res://Player/Cart.tscn")
+
+var player
+
 func _ready():
-	for n in $ObjectTilemap.generate_objects():
-		add_child(n)
+	generate()
+	create_cart()
+
+
+func generate():
+	var objs = $ObjectTilemap.generate_objects()
+	
+	for tile_name in objs:
+		for node in objs[tile_name]:
+			ysort.add_child(node)
+	
+	player = objs["player"][0]
+
+
+func create_cart():
+	var cart = cart_scene.instance()
+	cart.global_position = player.global_position
+	cart.target = player
+	ysort.add_child(cart)
