45afa84362cd10346d045e6859a14f17591ad701
ferdyferdy9 <ferdyferdy9514@gmail.com>
2020-10-30 23:10:39 +0700

    Update ObjectTilemap.gd

--- Levels/ObjectTilemap.gd
+++ Levels/ObjectTilemap.gd
@@ -1,26 +1,28 @@
 extends TileMap
 
-var box_instance = preload("res://Objects/Box.tscn")
+const packedScenes = {
+	"box": preload("res://Objects/Box.tscn"),
+	"player": preload("res://Player/Player.tscn"),
+}
 
 func _ready():
 	hide()
 
 
 func generate_objects():
-	var generated_objs = []
+	var generated_objs = {}
 	
 	for cell_pos in get_used_cells():
 		var cell_id = get_cellv(cell_pos)
 		var cell_name = tile_set.tile_get_name(cell_id)
 		var spawn_position = cell_pos * cell_size + cell_size * 0.5
 		
-		var obj:Node2D
-		match cell_name:
-			"box":
-				obj = box_instance.instance()
-		
-		if obj != null:
+		if packedScenes.has(cell_name):
+			var obj = packedScenes[cell_name].instance()
 			obj.global_position = spawn_position
-			generated_objs.append(obj)
+			
+			if not generated_objs.has(cell_name):
+				generated_objs[cell_name] = []
+			generated_objs[cell_name].append(obj)
 	
 	return generated_objs
