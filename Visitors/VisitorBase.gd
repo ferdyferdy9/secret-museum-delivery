6419f10977ecd6808c6faaf31ffdc6de95095989
ferdyferdy9 <ferdyferdy9514@gmail.com>
2020-11-01 10:31:35 +0700

    Update VisitorBase.gd

--- Visitors/VisitorBase.gd
+++ Visitors/VisitorBase.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 signal player_busted
 
-export var wander_max_distance = 10
+export var wander_max_distance = 50
 export var max_speed = 400
 
 onready var anim_player = $AnimationPlayer
@@ -10,24 +10,34 @@ onready var anim_player = $AnimationPlayer
 var wonder_target:Vector2
 var velocity:Vector2
 
+func _ready():
+	randomize()
+	wonder_target = global_position
+
+
 func _process(delta):
-	if velocity.x > 0:
+	if abs(velocity.angle_to(Vector2.RIGHT)) < PI*0.25:
 		anim_player.play("idle_right")
-	if velocity.x < 0:
+	if abs(velocity.angle_to(Vector2.LEFT)) < PI*0.25:
 		anim_player.play("idle_left")
-	if velocity.y > 0:
-		anim_player.play("idle_down")
-	if velocity.y < 0:
+	if abs(velocity.angle_to(Vector2.UP)) < PI*0.25:
 		anim_player.play("idle_up")
+	if abs(velocity.angle_to(Vector2.DOWN)) < PI*0.25:
+		anim_player.play("idle_down")
+	
+	print(velocity.angle_to(Vector2.RIGHT))
+	
+	if velocity.length_squared() != 0:
+		$FOV.rotation = velocity.angle()
 
 
 func _physics_process(delta):
 	go_to_desired_pos()
-	velocity = move_and_slide(velocity)
+	velocity = move_and_slide(velocity, Vector2.ZERO, false, 4, 0, false)
 
 
 func go_to_desired_pos():
-	velocity = global_position - wonder_target
+	velocity = wonder_target - global_position
 	if velocity.length_squared() > max_speed * max_speed:
 		velocity = wonder_target.normalized() * max_speed
 
@@ -35,8 +45,8 @@ func go_to_desired_pos():
 func _on_WanderTimer_timeout():
 	var offset:Vector2
 	
-	offset.x = randf() * wander_max_distance
-	offset.y = randf() * wander_max_distance
+	offset.x = rand_range(-1, 1) * wander_max_distance
+	offset.y = rand_range(-1, 1) * wander_max_distance
 	
 	wonder_target = global_position + offset
 
